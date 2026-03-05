cf85ec980b7d57aec8bc04eb74a8bb50f776f4c6
Diogo Almeida <diogocastroalmeida@proton.me>
2026-03-05 15:01:39 +0000

    finalizacao dos exercicios da pratica 2

---
+++ praticas/pratica2/Exercicio54.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int n = 1;
+    double x = 1.6785;
+
+    double prod = 1;
+    for (int i = 0; i < n; i++)
+    {
+        prod *= x;
+    }
+
+    return 0;
+}
